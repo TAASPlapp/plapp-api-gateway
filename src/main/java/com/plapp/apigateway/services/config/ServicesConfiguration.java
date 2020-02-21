1fcb9036ce16fe300aa041177523047b1a380f0d
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-02-21 13:24:58 +0100

    add services configuration beans

--- src/main/java/com/plapp/apigateway/services/config/ServicesConfiguration.java
+++ src/main/java/com/plapp/apigateway/services/config/ServicesConfiguration.java
@@ -1,4 +1,16 @@
 package com.plapp.apigateway.services.config;
 
+import com.plapp.apigateway.services.GreenhouseService;
+import com.plapp.apigateway.services.mockservices.MockGreenhouseService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.plapp.apigateway.controllers")
 public class ServicesConfiguration {
+    @Bean
+    public GreenhouseService getGreenhouseService() {
+        return new MockGreenhouseService();
+    }
 }

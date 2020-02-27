b7a084256ad43f251bd13a7fb234e4803f014f30
st148705 <valentino.dicianni@edu.unito.it>
2020-02-27 17:59:33 +0100

    jsonToStoryboard fixed

--- src/main/java/com/plapp/apigateway/controllers/SocialController.java
+++ src/main/java/com/plapp/apigateway/controllers/SocialController.java
@@ -17,36 +17,43 @@ public class SocialController {
     @Autowired
     private SocialService socialService;
 
+    @CrossOrigin
     @GetMapping("/user")
     public UserDetails getUserDetails(@RequestParam(defaultValue = "-1") long userId) throws Exception {
         return socialService.getUserDetails(userId);
     }
 
+    @CrossOrigin
     @PostMapping("/user/edit")
     public ApiResponse setUserDetails(@RequestBody UserDetails userDetails) throws Exception {
         return socialService.setUserDetails(userDetails);
     }
 
+    @CrossOrigin
     @GetMapping("/comments")
     public List<Comment> getComments(@RequestParam MediaContentType contentType, @RequestParam long itemId) throws  Exception {
         return socialService.getComments(contentType, itemId);
     }
 
+    @CrossOrigin
     @PostMapping("/comments/add")
     public ApiResponse addComment(@RequestBody Comment comment) throws Exception {
         return socialService.addComment(comment);
     }
 
+    @CrossOrigin
     @PostMapping("/likes/like")
     public ApiResponse like(@RequestBody MediaContentType contentType, @RequestParam long itemId) throws Exception {
         return socialService.addLike(new Like());
     }
 
+    @CrossOrigin
     @PostMapping("/likes/unlike")
     public ApiResponse unlike(@RequestBody long likeId) throws Exception {
         return socialService.unlike(likeId);
     }
 
+    @CrossOrigin
     @GetMapping("/likes")
     public List<UserDetails> getLikes(@RequestParam MediaContentType contentType, @RequestParam long itemId) throws Exception {
         return socialService.getLikes(contentType, itemId);
--- src/main/java/com/plapp/apigateway/services/mockservices/MockGreenhouseService.java
+++ src/main/java/com/plapp/apigateway/services/mockservices/MockGreenhouseService.java
@@ -14,23 +14,22 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.File;
 import java.io.FileReader;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 public class MockGreenhouseService implements GreenhouseService {
 
     private Plant jsonToPlant(JSONObject jsonPlant) {
-        Plant plant = new Plant((Long)jsonPlant.get("id"));
-        plant.setDescription((String)jsonPlant.get("description"));
-        plant.setOwner((Long)jsonPlant.get("owner"));
-        plant.setName((String)jsonPlant.get("name"));
-        plant.setType((String)jsonPlant.get("type"));
-        plant.setImage((String)jsonPlant.get("image"));
+        Plant plant = new Plant((Long) jsonPlant.get("id"));
+        plant.setDescription((String) jsonPlant.get("description"));
+        plant.setOwner((Long) jsonPlant.get("owner"));
+        plant.setName((String) jsonPlant.get("name"));
+        plant.setType((String) jsonPlant.get("type"));
+        plant.setImage((String) jsonPlant.get("image"));
         return plant;
     }
 
     private Storyboard jsonToStoryboard(JSONObject jsonStoryboard) {
-        Storyboard storyboard = new Storyboard((Long)jsonStoryboard.get("id"));
+        Storyboard storyboard = new Storyboard((Long) jsonStoryboard.get("id"));
         try {
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
             storyboard.setLastModified(formatter.parse((String) jsonStoryboard.get("lastModified")));
@@ -38,18 +37,30 @@ public class MockGreenhouseService implements GreenhouseService {
             System.out.println(e.getMessage());
         }
 
-        storyboard.setNumLikes(((Long)jsonStoryboard.get("numLikes")).intValue());
-        storyboard.setSummary((String)jsonStoryboard.get("summary"));
+        storyboard.setNumLikes(((Long) jsonStoryboard.get("numLikes")).intValue());
+        storyboard.setSummary((String) jsonStoryboard.get("summary"));
+
+        JSONObject obj = ((JSONObject) (jsonStoryboard.get("plant")));
+        Plant p = new Plant((Long) obj.get("id"),
+                (Long) obj.get("owner"),
+                (String) obj.get("name"),
+                (String) obj.get("description"),
+                (String) obj.get("type"),
+                Plant.PlantHealthStatus.valueOf(((String) obj.get("status")).toUpperCase()),
+                (String) obj.get("image"));
+
+        storyboard.setPlant(p);
 
         List<StoryboardItem> items = Lists.<JSONObject, StoryboardItem>map(
-                ((JSONArray)jsonStoryboard.get("storyboardItems")).iterator(),
+                ((JSONArray) jsonStoryboard.get("storyboardItems")).iterator(),
                 json -> {
-                    StoryboardItem item = new StoryboardItem((Long)json.get("id"));
-                    item.setImage((String)json.get("image"));
-                    item.setDescription((String)json.get("description"));
-                    item.setTitle((String)json.get("title"));
-                    item.setNumLikes(((Long)json.get("numLikes")).intValue());
-                    item.setStatus(Plant.PlantHealthStatus.valueOf(((String)json.get("status")).toUpperCase()));
+                    StoryboardItem item = new StoryboardItem((Long) json.get("id"));
+                    item.setImage((String) json.get("image"));
+                    item.setThumbImage((String) json.get("thumbImage"));
+                    item.setDescription((String) json.get("description"));
+                    item.setTitle((String) json.get("title"));
+                    item.setNumLikes(((Long) json.get("numLikes")).intValue());
+                    item.setStatus(Plant.PlantHealthStatus.valueOf(((String) json.get("status")).toUpperCase()));
                     return item;
                 }
         );
@@ -65,8 +76,8 @@ public class MockGreenhouseService implements GreenhouseService {
 
         File jsonFile = new ClassPathResource("mock-response/mock-plants.json").getFile();
         List<JSONObject> jsonPlants = Lists.<JSONObject>filter(
-                (JSONArray)parser.parse(new FileReader(jsonFile)),
-                p -> ((Long)p.get("owner")) == userId
+                (JSONArray) parser.parse(new FileReader(jsonFile)),
+                p -> ((Long) p.get("owner")) == userId
         );
 
         List<Plant> plants = Lists.map(
@@ -82,9 +93,9 @@ public class MockGreenhouseService implements GreenhouseService {
         JSONParser parser = new JSONParser();
 
         File jsonFile = new ClassPathResource("mock-response/mock-plants.json").getFile();
-        JSONObject jsonPlant = (JSONObject)Lists.<JSONObject>filter(
-                (JSONArray)parser.parse(new FileReader(jsonFile)),
-                p -> ((Long)p.get("id")) == plantId
+        JSONObject jsonPlant = (JSONObject) Lists.<JSONObject>filter(
+                (JSONArray) parser.parse(new FileReader(jsonFile)),
+                p -> ((Long) p.get("id")) == plantId
         ).get(0);
 
         return jsonToPlant(jsonPlant);
@@ -105,7 +116,7 @@ public class MockGreenhouseService implements GreenhouseService {
         JSONParser parser = new JSONParser();
 
         File jsonFile = new ClassPathResource("mock-response/mock-storyboard.json").getFile();
-        JSONArray jsonStoryboards = (JSONArray)parser.parse(new FileReader(jsonFile));
+        JSONArray jsonStoryboards = (JSONArray) parser.parse(new FileReader(jsonFile));
 
         List<Storyboard> storyboards = Lists.<JSONObject, Storyboard>map(
                 jsonStoryboards.iterator(),
@@ -122,7 +133,7 @@ public class MockGreenhouseService implements GreenhouseService {
         JSONArray storyboards = (JSONArray) parser.parse(new FileReader(jsonFile));
         List<JSONObject> jsonStoryboards = Lists.<JSONObject>filter(
                 storyboards.iterator(),
-                p -> ((Long)((JSONObject)p.get("plant")).get("id")) == plantId
+                p -> ((Long) ((JSONObject) p.get("plant")).get("id")) == plantId
         );
         return jsonToStoryboard(jsonStoryboards.get(0));
     }

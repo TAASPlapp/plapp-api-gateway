203b469bff56a059be73586bbbb1ca9fe6b0f2b9
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-03-05 13:40:12 +0100

    added plapp-authorization-filter dep

--- src/main/java/com/plapp/apigateway/services/AuthorizationService.java
+++ src/main/java/com/plapp/apigateway/services/AuthorizationService.java
@@ -1,8 +1,9 @@
 package com.plapp.apigateway.services;
 
+import com.plapp.authorization.ResourceAuthority;
 import com.plapp.entities.utils.ApiResponse;
 
 public interface AuthorizationService {
-    String addAuthorization(Long userId, String urlRegex, Long value);
-    String removeAuthorization(Long userId, String urlRegex, Long value);
+    String addAuthorization(ResourceAuthority authority);
+    String removeAuthorization(ResourceAuthority authority);
 }

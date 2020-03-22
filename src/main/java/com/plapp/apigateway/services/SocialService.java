cb0bf5b39f012054d56c1e48dbf8bddecbb14778
st148705 <valentino.dicianni@edu.unito.it>
2020-03-22 16:13:41 +0100

    bug fix

--- src/main/java/com/plapp/apigateway/services/SocialService.java
+++ src/main/java/com/plapp/apigateway/services/SocialService.java
@@ -4,22 +4,23 @@ import com.plapp.entities.social.Comment;
 import com.plapp.entities.social.Like;
 import com.plapp.entities.social.MediaContentType;
 import com.plapp.entities.social.UserDetails;
-import com.plapp.entities.utils.ApiResponse;
 
 import java.util.List;
 
 public interface SocialService {
     UserDetails getUserDetails(long userId) throws Exception;
 
-    ApiResponse addUserDetails(UserDetails user) throws Exception;
+    UserDetails addUserDetails(UserDetails user) throws Exception;
 
-    ApiResponse updateUserDetails(UserDetails userDetails) throws Exception;
+    UserDetails setUserDetails(UserDetails userDetails) throws Exception;
 
     List<Comment> getComments(MediaContentType type, long itemId) throws Exception;
 
     Comment addComment(Comment comment) throws Exception;
 
-    ApiResponse removeLike(long likeId) throws Exception;
+    Like addLike(Like like) throws Exception;
+
+    void removeLike(long likeId) throws Exception;
 
     List<UserDetails> getLikes(MediaContentType type, long itemId) throws Exception;
 }

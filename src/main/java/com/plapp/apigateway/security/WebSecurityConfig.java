e803c4ce96a0958691d46443f03d198b37b8a4d2
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-15 13:15:38 +0200

    remove comments & update web security

--- src/main/java/com/plapp/apigateway/security/WebSecurityConfig.java
+++ src/main/java/com/plapp/apigateway/security/WebSecurityConfig.java
@@ -92,17 +92,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/api/auth/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilter(new JWTAuthorizationFilter(authenticationManager(), authenticationService));
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .and();
-
+                .antMatchers("/api/auth/*").permitAll()
+                .antMatchers("/**").authenticated();
     }
 
 

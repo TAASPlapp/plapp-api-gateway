773a1f7543bd76fc242a0fdc83b4f6208dc64027
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-17 16:41:14 +0200

    add login orchestrator

---
+++ src/main/java/com/plapp/apigateway/saga/UserLoginSagaOrchestrator.java
@@ -0,0 +1,41 @@
+package com.plapp.apigateway.saga;
+
+import com.plapp.apigateway.saga.orchestration.SagaDefinition;
+import com.plapp.apigateway.saga.orchestration.SagaDefinitionBuilder;
+import com.plapp.apigateway.saga.orchestration.SagaExecutionEngine;
+import com.plapp.apigateway.saga.orchestration.SagaOrchestrator;
+import com.plapp.apigateway.services.SessionTokenService;
+import com.plapp.apigateway.services.microservices.AuthenticationService;
+import com.plapp.entities.auth.UserCredentials;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RequiredArgsConstructor
+public class UserLoginSagaOrchestrator extends SagaOrchestrator {
+    private static Logger logger = LoggerFactory.getLogger(UserLoginSagaOrchestrator.class);
+
+    private final AuthenticationService authenticationService;
+    private final SessionTokenService sessionTokenService;
+
+    @Override
+    protected SagaDefinition buildSaga(SagaDefinitionBuilder builder) {
+        logger.info("Building login saga orchestrator");
+
+        return builder
+                    .invoke(authenticationService::authenticateUser).withArg("inputCredentials").saveTo("jwt")
+                .step()
+                    .invoke(sessionTokenService::generateSessionToken).withArg("jwt").saveTo("sessionToken")
+                .step()
+                    .invoke(sessionTokenService::updateJwt).withArg("jwt")
+                .build();
+    }
+
+    public String authenticateUser(UserCredentials credentials) throws Throwable {
+        SagaExecutionEngine.SagaArgumentResolver resolver = getExecutor()
+                .withArg("inputCredentials", credentials)
+                .run()
+                .collect();
+        return resolver.get("sessionToken");
+    }
+}

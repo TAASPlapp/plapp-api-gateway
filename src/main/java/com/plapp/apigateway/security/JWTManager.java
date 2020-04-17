68687b4bcd35c53746927b51a6226956f5bd7d40
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-17 16:12:47 +0200

    fix readPublicKey

--- src/main/java/com/plapp/apigateway/security/JWTManager.java
+++ src/main/java/com/plapp/apigateway/security/JWTManager.java
@@ -15,6 +15,7 @@ import java.io.InputStream;
 import java.security.KeyFactory;
 import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 @Component
 @RequiredArgsConstructor
@@ -24,18 +25,21 @@ public class JWTManager {
     private PublicKey publicKey;
 
     @PostConstruct
-    public void readPublicKey() throws Exception {
-        InputStream inputStream = new ClassPathResource("private.der").getInputStream();
+    public void readPublicKey(String publicKeyPath) throws Exception {
+        InputStream inputStream = (new ClassPathResource(publicKeyPath)).getInputStream();
         DataInputStream dataInputStream = new DataInputStream(inputStream);
-        int availableBytes = dataInputStream.available();
 
-        logger.info("Loading private key from classpath, available bytes: " + availableBytes);
+        int availableBytes = dataInputStream.available();
+        logger.info("Loading public key from classpath, available bytes: " + availableBytes);
+
         byte[] keyBytes = new byte[availableBytes];
         dataInputStream.readFully(keyBytes);
 
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
         publicKey = keyFactory.generatePublic(keySpec);
+        logger.info("Public key loaded");
     }
 
 

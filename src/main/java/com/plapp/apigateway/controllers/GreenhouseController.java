38c7699bc4fdc668ebb02653798e4033a1d5646d
st148705 <valentino.dicianni@edu.unito.it>
2020-04-19 20:01:38 +0200

    minor fix

--- src/main/java/com/plapp/apigateway/controllers/GreenhouseController.java
+++ src/main/java/com/plapp/apigateway/controllers/GreenhouseController.java
@@ -18,39 +18,39 @@ public class GreenhouseController {
     private final GreenhouseService greenhouseService;
 
     @GetMapping(value= {"/plants", "/{userId}/plants"})
-    public ApiResponse<List<Plant>> getPlants(@PathVariable(name="userId", required = false) Optional<Long> optId) throws Exception {
+    public ApiResponse<List<Plant>> getPlants(@PathVariable(name="userId", required = false) Optional<Long> optId) {
         long userId = optId.orElse(-1L);
         return new ApiResponse<>(greenhouseService.getPlants(userId));
     }
 
     @GetMapping("/plant/{plantId}")
-    public ApiResponse<Plant> getPlant(@PathVariable(name = "plantId") long plantId) throws Exception {
+    public ApiResponse<Plant> getPlant(@PathVariable(name = "plantId") long plantId) {
         return new ApiResponse<>(greenhouseService.getPlant(plantId));
     }
 
     //todo: add plant
     @PostMapping("/plants/add")
-    public ApiResponse<Plant> addPlant(@RequestBody Plant plant) throws Exception {
+    public ApiResponse<Plant> addPlant(@RequestBody Plant plant) {
         return new ApiResponse<>(greenhouseService.addPlant(plant));
     }
 
 
     @GetMapping("/storyboards/feed")
-    public ApiResponse<List<Storyboard>> getStoryboards() throws Exception {
+    public ApiResponse<List<Storyboard>> getStoryboards() {
         return new ApiResponse<>(greenhouseService.getStoryboards());
     }
 
 
     //TODO: storyboard di un utente specifico -> per mostare il profilo
     @GetMapping(value = {"/storyboards", "/storyboards/{userId}"})
-    public ApiResponse<List<Storyboard>> getStoryboards(@PathVariable(name = "userId") Optional<Long> optId) throws Exception {
+    public ApiResponse<List<Storyboard>> getStoryboards(@PathVariable(name = "userId") Optional<Long> optId) {
         Long userId = optId.orElse(-1L);
         return new ApiResponse<>(greenhouseService.getStoryboards());
         //return greenhouseService.getStoryboards(userId)
     }
 
     @GetMapping("/plant/{plantId}/storyboard")
-    public ApiResponse<Storyboard> getStoryboard(@PathVariable long plantId) throws Exception {
+    public ApiResponse<Storyboard> getStoryboard(@PathVariable long plantId) {
         return new ApiResponse<>(greenhouseService.getStoryboard(plantId));
     }
 }

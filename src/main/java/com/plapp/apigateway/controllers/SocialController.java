c4f957975cddca7239f39b5f26c1d1a11c87478d
st148705 <valentino.dicianni@edu.unito.it>
2020-04-19 21:22:31 +0200

    SocialController bug fix

--- src/main/java/com/plapp/apigateway/controllers/SocialController.java
+++ src/main/java/com/plapp/apigateway/controllers/SocialController.java
@@ -1,5 +1,6 @@
 package com.plapp.apigateway.controllers;
 
+import com.plapp.apigateway.services.config.SessionRequestContext;
 import com.plapp.apigateway.services.microservices.SocialService;
 import com.plapp.entities.social.Comment;
 import com.plapp.entities.social.Like;
@@ -19,13 +20,15 @@ public class SocialController {
 
     @CrossOrigin
     @GetMapping("/user")
-    public ApiResponse<UserDetails> getUserDetails(@RequestParam(defaultValue = "-1") long userId) throws Exception {
+    public ApiResponse<UserDetails> getUserDetails(@RequestParam(defaultValue = "-1") long userId) {
+        if (userId == -1L)
+            userId = SessionRequestContext.getCurrentUserId();
         return new ApiResponse<>(socialService.getUserDetails(userId));
     }
 
     @CrossOrigin
     @PostMapping("/user/edit")
-    public ApiResponse<UserDetails> setUserDetails(@RequestBody UserDetails userDetails) throws Exception {
+    public ApiResponse<UserDetails> setUserDetails(@RequestBody UserDetails userDetails) {
         return new ApiResponse<>(socialService.setUserDetails(userDetails));
 
     }
@@ -33,31 +36,31 @@ public class SocialController {
     @CrossOrigin
     @GetMapping("/comments")
     public List<Comment> getComments(@RequestParam MediaContentType contentType,
-                                     @RequestParam long itemId) throws Exception {
+                                     @RequestParam long itemId) {
         return socialService.getComments(contentType, itemId);
     }
 
     @CrossOrigin
     @PostMapping("/comments/add")
-    public ApiResponse<Comment> addComment(@RequestBody Comment comment) throws Exception {
+    public ApiResponse<Comment> addComment(@RequestBody Comment comment) {
         return new ApiResponse<>(socialService.addComment(comment));
     }
 
     @CrossOrigin
     @PostMapping("/likes/like")
-    public ApiResponse<Like> like(@RequestBody Like like) throws Exception {
+    public ApiResponse<Like> like(@RequestBody Like like) {
         return new ApiResponse<>(socialService.addLike(like));
     }
 
     @CrossOrigin
     @PostMapping("/likes/unlike")
-    public void unlike(@RequestBody long likeId) throws Exception {
+    public void unlike(@RequestBody long likeId) {
         socialService.removeLike(likeId);
     }
 
     @CrossOrigin
     @GetMapping("/likes")
-    public ApiResponse<List<UserDetails>> getLikes(@RequestParam MediaContentType contentType, @RequestParam long itemId) throws Exception {
+    public ApiResponse<List<UserDetails>> getLikes(@RequestParam MediaContentType contentType, @RequestParam long itemId) {
         return new ApiResponse<>(socialService.getLikes(contentType, itemId));
     }
 }

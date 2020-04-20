e07a0f9b7f36aca1a838d9a247f09528f8d4f71b
gegelumis <matteo.dicunzolo@edu.unito.it>
2020-04-20 23:17:07 +0200

    Aggiunto metodo getDiagnosis

--- src/main/java/com/plapp/apigateway/services/microservices/GardenerService.java
+++ src/main/java/com/plapp/apigateway/services/microservices/GardenerService.java
@@ -5,9 +5,9 @@ import com.plapp.entities.schedules.ScheduleAction;
 import java.util.List;
 
 public interface GardenerService {
-    List<ScheduleAction> getSchedule(long plantId) ;
+    List<ScheduleAction> getSchedules(long plantId) ;
     List<String> getActions() ;
-
+    String getDiagnosis(String plantImageURL, String plantId);
     ScheduleAction addScheduleAction(ScheduleAction scheduleAction) ;
     void removeScheduleAction(ScheduleAction scheduleAction) ;
 }

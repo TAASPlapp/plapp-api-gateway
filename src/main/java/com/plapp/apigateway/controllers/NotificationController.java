6e290bfcd94841dea0471b1bde66da96d9eb51cd
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-20 17:18:48 +0200

    fix typo

--- src/main/java/com/plapp/apigateway/controllers/NotificationController.java
+++ src/main/java/com/plapp/apigateway/controllers/NotificationController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/notification")
+@RequestMapping("api/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
     final NotificationService notificationService;
